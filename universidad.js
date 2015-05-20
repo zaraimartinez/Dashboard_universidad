c841eed836f870946fecc4fb2481ecaf26ca2ed6
zaraimartinez <zarai.martinezt@gmail.com>
2015-05-20 14:14:17 -0500

    espacios

---
+++ universidad.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".circulo-externo").hover(function(){
+    $(".home-metodologia").toggleClass("hidden");
+    $(".hover-metodologia").toggleClass("hidden");
+  });
+});
