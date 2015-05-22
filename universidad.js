17233fe79cbeefcdf7093661e51ab9ff725c054a
zaraimartinez <zarai.martinezt@gmail.com>
2015-05-22 18:14:17 -0500

    efecto de eyes - toggle

--- universidad.js
+++ universidad.js
@@ -1,6 +1,24 @@
-$(document).ready(function(){
-  $(".img-girl").hover(function(){
-    $(".hover-img").toggleClass("hidden");
-    $(".drop-menu-perfil").toggleClass("hidden");
+$( document ).ready(function(){
+  //$(".img-girl").hover(function(){
+    //$(".hover-img").toggleClass("hidden");
+    //$(".drop-menu-perfil").toggleClass("hidden");
+  //});
+
+  $(".open-eye").click(function(){
+  	$(this).parent().find(".faltas").toggle();
+  	$(this).parent().find(".promedios").toggle();
+  	$(this).hide();
   });
+	  	$(".faltas").click(function(){
+		  	$(this).parent().find(".open-eye").toggle();
+		  	$(this).parent().find(".promedios").toggle();
+		  	$(this).hide();
+	});
+	  	$(".promedios").click(function(){
+		  	$(this).parent().find(".faltas").toggle();
+		  	$(this).parent().find(".promedios").toggle();
+		  	$(this).hide();
+	});
+
+
 });
